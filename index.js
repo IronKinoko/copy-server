23b89fe5313f98cf892cdc3835f61f380e5b5acf
IronKinoko <kinoko_main@outlook.com>
2022-05-14 11:08:18 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const clipboard = require('clipboardy')
+const express = require('express')
+const parser = require('body-parser')
+const app = express()
+
+app.use(parser.json())
+app.use(parser.urlencoded())
+
+const PORT = process.env.PORT || 5555
+
+app.get('/copy', (req, res) => {
+  try {
+    res.send(clipboard.readSync())
+  } catch {
+    res.sendStatus(200)
+  }
+})
+
+app.post('/paste', (req, res) => {
+  clipboard.writeSync(req.body.data)
+  res.send(req.body)
+})
+
+app.listen(PORT, () =>
+  console.log(`sever run in http://localhost:${PORT} ${new Date().toString()}`)
+)
